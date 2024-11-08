177ddae4deed4ebf217c04e214cbc68a9665ff31
navidul hoque <hnavidul@gmail.com>
2024-11-08 12:14:46 +0600

    backend

---
+++ src/models/User.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: [true, "Username already exists"],
+        trim: true,
+        minLength: [5, 'Username must be at least 5 characters long'],
+        maxLength: [15, 'Username cannot exceed 15 characters'],
+        match: [/^[a-zA-Z0-9]+$/, 'Username can only contain alphanumeric characters (no special characters and space are allowed)'],
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: [true, "Email already exists"],
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+    },
+    
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: [8, 'Password must be at least 8 characters long'],
+        match: [
+            /^(?=.*\d)(?=.*[\W_]).{8,}$/,
+            'Password must contain at least one number and one special character',
+        ],
+    },
+
+},{timestamps: true})
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
-import express from "express"
-import { PORT } from "./config/config"
+import express, { Request, Response } from "express"
+import { FRONTEND_URL, PORT } from "./config/config"
 import connectDatabase from "./connectDatabase"
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}))
+
+app.use(cookieParser())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("API Working");
+});
 
 async function startServer() {
     await connectDatabase()
