c58b3dc0495fccd5f77f9f546cd16ae4768da289
navidul hoque <hnavidul@gmail.com>
2024-11-16 14:49:14 +0600

    backend

--- controller/auth.js
+++ controller/auth.js
@@ -4,32 +4,36 @@ import { CLERK_WEBHOOK_SECRET } from '../config/config.js';
 
 export const clerkWebHooks = async (req, res) => {
 
+    // Create new Svix instance with secret
+    const wh = new Webhook(CLERK_WEBHOOK_SECRET)
+
+    // Get headers and body
+    const headers = req.headers
+    const payload = req.body
+
+    // Get Svix headers for verification
+    const svix_id = headers['svix-id']
+    const svix_timestamp = headers['svix-timestamp']
+    const svix_signature = headers['svix-signature']
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return void res.status(400).json({
+            success: false,
+            message: 'Error: Missing svix headers',
+        })
+    }
+
+    let evt
+
     try {
-        const headers = req.headers
-        const payload = req.body
-
-        // Get the Svix headers for verification
-        const svix_id = headers['svix-id']
-        const svix_timestamp = headers['svix-timestamp']
-        const svix_signature = headers['svix-signature']
-
-        if (!svix_id || !svix_timestamp || !svix_signature) {
-
-            return res.json({
-                status: false,
-                message: "Error occured -- no svix headers"
-            })
-        }
-
-        const webHook = new Webhook(CLERK_WEBHOOK_SECRET)
-
-        await webHook.verify(JSON.stringify(payload), {
+        evt = wh.verify(payload, {
             'svix-id': svix_id,
             'svix-timestamp': svix_timestamp,
             'svix-signature': svix_signature,
         })
 
-        const { data, type } = payload
+        const { data, type } = evt
 
         console.log(type)
 
