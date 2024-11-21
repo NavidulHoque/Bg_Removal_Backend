0cd9dd16c9e7ddd972c314b34c74eb85053bdb5c
navidul hoque <hnavidul@gmail.com>
2024-11-21 14:38:34 +0600

    backend

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.post("/registration", createAnUser)
 
-router.post("/login", loginAnUse)
+router.post("/login", loginAnUser)
 
 router.get("/logout", logoutAnUser)
 
