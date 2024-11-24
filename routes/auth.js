2d521982f398ed85c84868c2aba3048b020060d7
navidul hoque <hnavidul@gmail.com>
2024-11-24 18:01:45 +0600

    backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import { createAnUser, loginAnUser, logoutAnUser } from '../controller/auth.js'
+import { createAnUser, loginAnUser } from '../controller/auth.js'
 
 const router = express.Router()
 
 router.post("/registration", createAnUser)
 
 router.post("/login", loginAnUser)
-
-router.get("/logout", logoutAnUser)
 
 export default router
